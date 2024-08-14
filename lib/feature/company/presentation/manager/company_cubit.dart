8878a55764a3a0387810225d70abf9d20c3a972f
Moustafa Mohamed <moustafamohmed203@gmail.com>
2024-08-14 05:10:06 +0300

    company offer and cashback logic done

--- lib/feature/company/presentation/manager/company_cubit.dart
+++ lib/feature/company/presentation/manager/company_cubit.dart
@@ -6,7 +6,6 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
-
 import '../../../../core/functions/get_random_number.dart';
 import '../../../../core/utils/common.dart';
 import '../../data/company_model.dart';
